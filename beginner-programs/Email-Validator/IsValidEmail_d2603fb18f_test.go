690a0ee407ec024062dc7affc57029802f8d27a1
SHAKULMITTAL22 <80421896+SHAKULMITTAL22@users.noreply.github.com>
2023-09-22 10:01:56 +0000

    Test generated by RoostGPT for test azure-32k-go using AI Type Azure Open AI and AI Model roost-gpt4-32k, for user feedback: add more test examples

--- beginner-programs/Email-Validator/IsValidEmail_d2603fb18f_test.go
+++ beginner-programs/Email-Validator/IsValidEmail_d2603fb18f_test.go
@@ -19,6 +19,11 @@ func TestIsValidEmail_d2603fb18f(t *testing.T) {
 		{"testemail@gmail.com", true},
 		{"invalidemail@invalid", false},
 		{"test@com", false},
+        {"test@invalid..com", false}, // new test case: multiple periods are not allowed after @ 
+        {"test.@invalid.com", false}, // new test case: period is not allowed after local part
+        {"test..test@invalid.com", false}, // new test case: multiple periods are not allowed in local part 
+        {"test.test@invalid.com", true}, // new test case: single period is allowed in local part
+        {"test.test@invalid.co.in", true}, // new test case: mulitple period allowed after @ 
 	}
 
 	// Running test cases
