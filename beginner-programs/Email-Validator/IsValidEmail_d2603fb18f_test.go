b8e64194524c301d7a967ac606ca9de4cc62cbcc
SHAKULMITTAL22 <80421896+SHAKULMITTAL22@users.noreply.github.com>
2023-09-22 10:06:56 +0000

    Test generated by RoostGPT for test vertex-32k-golang-unit using AI Type Vertex AI and AI Model code-bison-32k

---
+++ beginner-programs/Email-Validator/IsValidEmail_d2603fb18f_test.go
@@ -0,0 +1,107 @@
+package Validator
+
+import (
+	"testing"
+)
+
+func TestIsValidEmail_d2603fb18f(t *testing.T) {
+	// Test case 1: Valid email address
+	email := "example@example.com"
+	expected := true
+	actual := IsValidEmail(email)
+	if actual != expected {
+		t.Errorf("Expected %t, got %t for email %s", expected, actual, email)
+	} else {
+		t.Log("Test case 1 passed")
+	}
+
+	// Test case 2: Invalid email address
+	email = "example"
+	expected = false
+	actual = IsValidEmail(email)
+	if actual != expected {
+		t.Errorf("Expected %t, got %t for email %s", expected, actual, email)
+	} else {
+		t.Log("Test case 2 passed")
+	}
+
+	// Test case 3: Email address with invalid characters
+	email = "example@example.com!"
+	expected = false
+	actual = IsValidEmail(email)
+	if actual != expected {
+		t.Errorf("Expected %t, got %t for email %s", expected, actual, email)
+	} else {
+		t.Log("Test case 3 passed")
+	}
+
+	// Test case 4: Email address with invalid domain
+	email = "example@example"
+	expected = false
+	actual = IsValidEmail(email)
+	if actual != expected {
+		t.Errorf("Expected %t, got %t for email %s", expected, actual, email)
+	} else {
+		t.Log("Test case 4 passed")
+	}
+
+	// Test case 5: Email address with valid TLD
+	email = "example@example.co.uk"
+	expected = true
+	actual = IsValidEmail(email)
+	if actual != expected {
+		t.Errorf("Expected %t, got %t for email %s", expected, actual, email)
+	} else {
+		t.Log("Test case 5 passed")
+	}
+
+	// Test case 6: Email address with valid subdomain
+	email = "example@subdomain.example.com"
+	expected = true
+	actual = IsValidEmail(email)
+	if actual != expected {
+		t.Errorf("Expected %t, got %t for email %s", expected, actual, email)
+	} else {
+		t.Log("Test case 6 passed")
+	}
+
+	// Test case 7: Email address with valid domain and subdomain
+	email = "example@subdomain.example.co.uk"
+	expected = true
+	actual = IsValidEmail(email)
+	if actual != expected {
+		t.Errorf("Expected %t, got %t for email %s", expected, actual, email)
+	} else {
+		t.Log("Test case 7 passed")
+	}
+
+	// Test case 8: Email address with valid domain and TLD
+	email = "example@example.com.uk"
+	expected = true
+	actual = IsValidEmail(email)
+	if actual != expected {
+		t.Errorf("Expected %t, got %t for email %s", expected, actual, email)
+	} else {
+		t.Log("Test case 8 passed")
+	}
+
+	// Test case 9: Email address with valid domain, subdomain and TLD
+	email = "example@subdomain.example.com.uk"
+	expected = true
+	actual = IsValidEmail(email)
+	if actual != expected {
+		t.Errorf("Expected %t, got %t for email %s", expected, actual, email)
+	} else {
+		t.Log("Test case 9 passed")
+	}
+
+	// Test case 10: Email address with valid domain, subdomain, TLD and ccTLD
+	email = "example@subdomain.example.co.uk"
+	expected = true
+	actual = IsValidEmail(email)
+	if actual != expected {
+		t.Errorf("Expected %t, got %t for email %s", expected, actual, email)
+	} else {
+		t.Log("Test case 10 passed")
+	}
+}
