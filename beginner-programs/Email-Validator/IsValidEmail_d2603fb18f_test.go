1e610379beb4f10d58419b695dd019eb853a89ac
SHAKULMITTAL22 <80421896+SHAKULMITTAL22@users.noreply.github.com>
2023-09-25 06:04:41 +0000

    Test generated by RoostGPT for test azure-32k-go using AI Type Azure Open AI and AI Model roost-gpt4-32k, for user feedback: Add more example test cases for above code

--- beginner-programs/Email-Validator/IsValidEmail_d2603fb18f_test.go
+++ beginner-programs/Email-Validator/IsValidEmail_d2603fb18f_test.go
@@ -5,24 +5,29 @@ import (
 )
 
 func TestIsValidEmail(t *testing.T) {
-	//Test case for valid email
-	email := "test@example.com"
-	expected := true
-	if result := IsValidEmail(email); result != expected {
-		t.Errorf("TestIsValidEmail failed with Email: %s. Got: %v, expected: %v", email, result, expected)
-	} 
-
-	//Test case for invalid email
-	email = "test@"
-	expected = false
-	if result := IsValidEmail(email); result != expected {
-		t.Errorf("TestIsValidEmail failed with Email: %s. Got: %v, expected: %v", email, result, expected)
+	testCases := []struct {
+		email    string
+		expected bool
+	}{
+		//Test case for valid email
+		{"test@example.com", true},
+		//Test case for invalid email
+		{"test@", false},
+		//Test case for email length > 254
+		{"a@b.c" + string(make([]byte, 253)), false},
+		//Test case for email without domain
+		{"test@", false},
+		//Test case for email without @ symbol
+		{"testexample.com", false},
+		//Test case for email with special characters in local part
+		{"test..test@example.com", false},
+		//Test case for email with special characters in domain part
+		{"test@exa$mple.com", false},
 	}
 
-	//Test case for email length > 254
-	email = "a@b.c" + string(make([]byte, 253))
-	expected = false
-	if result := IsValidEmail(email); result != expected {
-		t.Errorf("TestIsValidEmail failed with Email: %s. Got: %v, expected: %v", email, result, expected)
+	for _, testCase := range testCases {
+		if result := IsValidEmail(testCase.email); result != testCase.expected {
+			t.Errorf("TestIsValidEmail failed with Email: %s. Got: %v, expected: %v", testCase.email, result, testCase.expected)
+		}
 	}
 }
