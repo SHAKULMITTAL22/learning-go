adcaf46aa6efd71250e76e972b526247888cde77
SHAKULMITTAL22 <80421896+SHAKULMITTAL22@users.noreply.github.com>
2024-10-15 14:01:18 +0530

    Update pascal_test.go

--- algorithms/math/PascalTriangle/pascal_test.go
+++ algorithms/math/PascalTriangle/pascal_test.go
@@ -65,6 +65,67 @@ func TestIsPascalTriangle(t *testing.T) {
 	})
 }
 
+func TestIsPascal2Triangle(t *testing.T) {
+	t.Run("PascalTraingle of 1", func(t *testing.T) {
+		got := pascalTriangle(1)
+		want := [][]int{
+			{1},
+		}
+
+		if len(got) != len(want) {
+			t.Errorf("Different array size then expected")
+		}
+
+		for i := 0; i < len(got); i++ {
+			if !Equal(got[i], want[i]) {
+				t.Errorf("Error")
+			}
+		}
+
+	})
+
+	t.Run("PascalTraingle of 2", func(t *testing.T) {
+		got := pascalTriangle(2)
+		want := [][]int{
+			{1},
+			{1, 1},
+		}
+
+		if len(got) != len(want) {
+			t.Errorf("Different array size then expected")
+		}
+
+		for i := 0; i < len(got); i++ {
+			if !Equal(got[i], want[i]) {
+				t.Errorf("Error")
+			}
+		}
+
+	})
+
+	t.Run("PascalTraingle of 5", func(t *testing.T) {
+		got := pascalTriangle(5)
+		want := [][]int{
+			{1},
+			{1, 1},
+			{1, 2, 1},
+			{1, 3, 3, 1},
+			{1, 4, 6, 4, 1},
+		}
+
+		if len(got) != len(want) {
+			t.Errorf("Different array size then expected")
+		}
+
+		for i := 0; i < len(got); i++ {
+			if !Equal(got[i], want[i]) {
+				t.Errorf("Error")
+			}
+		}
+
+	})
+}
+
 func Equal(a, b []int) bool {
 	if len(a) != len(b) {
 		return false
